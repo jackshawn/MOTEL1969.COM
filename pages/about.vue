8c2be2cd1852d126a527323037b040f6e487a136
jackshawn <jackshawn@live.com>
2018-10-17 14:28:25 +0800

    加入建设中图标

--- pages/about.vue
+++ pages/about.vue
@@ -1,12 +1,16 @@
 <template>
     <div class="about">
-        <p style="text-align: center">coming soon..</p>
+        <p class="yet"><Icon icon="coming"></Icon></p>
     </div>
 </template>
 
 <script>
+    import Icon from '~/components/Icon'
     export default {
         name: 'about',
+        components: {
+            Icon
+        },
         data() {
             return {}
         },
@@ -14,5 +18,14 @@
 </script>
 <style scoped lang="scss">
     .about {
+        .yet {
+            margin-top: 80px;
+            text-align: center;
+
+            &>svg {
+                width: 60px;
+                height: 60px;
+            }
+        }
     }
 </style>
