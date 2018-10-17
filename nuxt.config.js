3b40b5261e5cc93a05c53c9786fb36ceaf7e3c32
jackshawn <jackshawn@live.com>
2018-10-17 12:01:37 +0800

    加入百度统计

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,55 +1,67 @@
-const pkg = require('./package')
+const movie = require('./assets/data')
+let name = (function() {
+    let arr = []
+    movie.forEach(function(i) {
+        i.movie.forEach(function(j) {
+            arr.push(j.name)
+        })
+    })
+    return arr
+})();
 
 module.exports = {
-  mode: 'universal',
+    mode: 'universal',
 
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: pkg.name,
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
-
-  /*
-  ** Global CSS
-  */
-  css: [
-  ],
-
-  /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
-
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
-  ],
-
-  /*
-  ** Build configuration
-  */
-  build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-      
+     ** Headers of the page
+     */
+    head: {
+        title: 'MOTEL1969',
+        meta: [
+            {charset: 'utf-8'},
+            {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+            {hid: 'description', name: 'description', content: 'MOTEL1969剪辑的所有电影资源,BGM资源,好片分享'},
+            {hid: 'keywords', name: 'keywords', content: '经典电影,热门电影,好看的电影,百度云电影,' + name.toString()},
+        ],
+        script: [
+            {src: 'https://hm.baidu.com/hm.js?07c201f86bb993f888b4c9ec71ce4264'}, /*引入百度统计的js*/
+        ],
+        link: [
+            {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
+        ]
+    },
+
+    /*
+     ** Customize the progress-bar color
+     */
+    loading: {color: '#fff'},
+
+    /*
+     ** Global CSS
+     */
+    css: [],
+
+    /*
+     ** Plugins to load before mounting the App
+     */
+    plugins: [
+        '@/assets/baidu.js'
+    ],
+
+    /*
+     ** Nuxt.js modules
+     */
+    modules: [],
+
+    /*
+     ** Build configuration
+     */
+    build: {
+        /*
+         ** You can extend webpack config here
+         */
+        extend(config, ctx) {
+
+        }
     }
-  }
 }
